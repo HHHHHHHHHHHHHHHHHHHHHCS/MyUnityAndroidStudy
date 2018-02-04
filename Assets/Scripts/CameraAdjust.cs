74cb7c2da4c29a38bab9828b39b4885e2fe952b5
HCS <464962683@qq.com>
2018-02-04 23:12:14 +0800

    自适应学习

---
+++ Assets/Scripts/CameraAdjust.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 用于自适应， 只对正交相机有用， 权重是 宽度
+/// </summary>
+public class CameraAdjust : MonoBehaviour
+{
+    public float initOrthoSize, initWidth, initHeight;
+    private float factOrthSize, factWidth, factHeight;
+
+    private void Update()
+    {
+        factWidth = Screen.width;
+        factHeight = Screen.height;
+        factOrthSize = (initOrthoSize * (initWidth / initHeight)) / (factWidth / factHeight);
+        GetComponent<Camera>().orthographicSize = factOrthSize;
+    }
+
+}

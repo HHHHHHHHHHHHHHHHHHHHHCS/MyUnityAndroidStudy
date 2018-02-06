14878df4189b4e3a1133e389914d51e2e127e21c
HCS <464962683@qq.com>
2018-02-07 00:10:31 +0800

    更新了拖尾的效果，注意 ScreenToWorld 要加距离 Z

---
+++ Assets/Scripts/Trail.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    public GameObject trailPrefab;
+    private Transform markTS;
+
+
+    private void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch t0 = Input.touches[0];
+            switch (t0.phase)
+            {
+                //当手指刚按下的时候，对应的Touch 对象的状态是Began
+                case TouchPhase.Began:
+                    if (markTS)
+                    {
+                        Destroy(markTS.gameObject);
+                    }
+                    //position是手指所对应屏幕的实时坐标
+                    //rawPosition是手指所对应屏幕的 初始坐标，即刚按下的时候
+                    markTS = Instantiate(trailPrefab, Camera.main.ScreenToWorldPoint(new Vector3(t0.position.x, t0.position.y, 10)), Quaternion.identity).transform;
+                    break;
+                //手指移动
+                case TouchPhase.Moved:
+                    markTS.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(t0.position.x, t0.position.y, 10));
+                    break;
+                //当手指按住不动的时候
+                case TouchPhase.Stationary:
+                    markTS.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(t0.position.x, t0.position.y, 10));
+                    break;
+                //当手指离开屏幕的时候
+                case TouchPhase.Ended:
+                    Destroy(markTS.gameObject);
+                    break;
+                //当手指因为某些原因出现问题的时候，多半是系统原因，取消对某个手指的追踪
+                case TouchPhase.Canceled:
+                    Destroy(markTS.gameObject);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

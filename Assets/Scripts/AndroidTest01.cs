1d45c3dc63465d6e0bc76746e01f054487267be4
HCS <464962683@qq.com>
2018-02-11 23:09:48 +0800

    重新导入安卓的JAR包，成功导出安卓包，并且运行成功

---
+++ Assets/Scripts/AndroidTest01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidTest01 : MonoBehaviour
+{
+
+    public Text text;
+
+	void Start ()
+    {
+        //获得包名
+        AndroidJavaClass ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        //获得ajc所代表的类下面的对象
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        text.text = ajo.Call<double>("Add", 1.1d, 2.2d).ToString();
+
+	}
+
+
+}

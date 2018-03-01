a83c17d171cb898c470a451df4becd45858552c9
HCS <464962683@qq.com>
2018-03-01 20:42:20 +0800

    Toast制作，但是没有测试

---
+++ Assets/Scripts/AndroidTest04.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidTest04 : MonoBehaviour
+{
+    private void Start()
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaClass toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            toast.CallStatic<AndroidJavaObject>("makeText", context, "Send From Unity", toast.GetStatic<int>("LENGTH_LONG")).Call("show");
+        }));
+    }
+}
